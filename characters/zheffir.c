5bd763bc5b09ce6977d494ad2349e584aacd69b9
Noah Gordon <noahzacharygordon@gmail.com>
2019-10-05 18:06:53 -0400

    Reorganize

---
+++ characters/zheffir.c
@@ -0,0 +1,12 @@
+name: Zheffir Stormcaller
+
+quests:
+- dragon_eggs
+
+description: A hardened sandsailor. Tan, leathery, dusty skin. Keeps most of his skin covered at all times to protect from the sand. Enjoys bathing, smoking and relaxing at Sahada Oasis when not out on journey across the Sea of Sands. Secretly a member of the Cerulean Society.
+
+Burly, dark hair, waxed moustache, dirty, smells of booze and tobacco, scarred
+
+Perception 16: tattoo of blue dragon on his right calf
+
+To Nephi: “You are High Hamut blood, yes? You are a capable fighter?”
